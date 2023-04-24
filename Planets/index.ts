735724adec76cf7dc42f6458f30d838473ce7fac
Ouzx <oguzhankandakoglu@gmail.com>
2023-04-25 01:55:24 +0300

    routes added

--- Planets/index.ts
+++ Planets/index.ts
@@ -1,13 +1,20 @@
 import dotenv from "dotenv";
-import express from "express";
-// import morgan
+import express, { Request, Response } from "express";
 import morgan from "morgan";
+// import joi
+import Joi from "joi";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.use(morgan("dev"));
+
+const planetsSchema = Joi.object({
+  id: Joi.number().required(),
+  name: Joi.string().required(),
+});
 
 type Planet = {
   id: number;
@@ -35,6 +42,70 @@ app.post("/planets", (req, res) => {
   const { planet } = req.body;
   planets.push(planet);
   res.send("Planet added");
+});
+
+/**
+ * - Write a router with the following routes:
+  - `GET /api/planets`: return all planets (JSON) with `200`
+  - `GET /api/planets/:id`: return a planet (JSON) by id with `200`
+  - `POST /api/planets`: create a planet, return only `201` code and a success JSON with key `msg`
+    - Make sure every planet is created with `id` and `name`.
+  - `PUT /api/planets/:id`: update a planet by id, return only `200` code and a success JSON with key `msg`
+  - `DELETE /api/planets/:id`: delete a planet by id, return only `200` code and a success JSON with key `msg`
+- Validate planet fields where appropriate.
+ */
+
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+});
+
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((planet) => planet.id === Number(id));
+  if (planet) {
+    res.status(200).json(planet);
+  } else {
+    res.status(404).json({ msg: "Planet not found" });
+  }
+});
+
+app.post("/api/planets", (req: Request, res: Response) => {
+  const { planet } = req.body;
+  const { error } = planetsSchema.validate(planet);
+  if (error) {
+    res.status(400).json({ msg: error.details[0].message });
+  } else {
+    planets.push(planet);
+    res.status(201).json({ msg: "Planet added" });
+  }
+});
+
+app.put("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { planet } = req.body;
+  const { error } = planetsSchema.validate(planet);
+  if (error) {
+    res.status(400).json({ msg: error.details[0].message });
+  } else {
+    const planetIndex = planets.findIndex((planet) => planet.id === Number(id));
+    if (planetIndex !== -1) {
+      planets[planetIndex] = planet;
+      res.status(200).json({ msg: "Planet updated" });
+    } else {
+      res.status(404).json({ msg: "Planet not found" });
+    }
+  }
+});
+
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planetIndex = planets.findIndex((planet) => planet.id === Number(id));
+  if (planetIndex !== -1) {
+    planets.splice(planetIndex, 1);
+    res.status(200).json({ msg: "Planet deleted" });
+  } else {
+    res.status(404).json({ msg: "Planet not found" });
+  }
 });
 
 app.listen(process.env.PORT || 3000, () => {
