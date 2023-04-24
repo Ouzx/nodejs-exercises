171d077ec72fad052b37c58270b66d6c0b92b12e
Ouzx <oguzhankandakoglu@gmail.com>
2023-04-25 01:07:00 +0300

    ex-4

--- 3/04-01-server.mjs
+++ 3/04-01-server.mjs
@@ -8,7 +8,7 @@ const server = createServer((request, response) => {
   response.setHeader("Content-Type", "text/html");
 
   response.end(
-    "<html><body><h1>This page was served with Node.js!</h1></body></html>"
+    "<html><body><h1>This page was served with Node.js! HELLO EVERYONE!</h1></body></html>"
   );
 });
 
