ec99a6a7499213313fd63b417b959a7c24845c67
Ouzx <oguzhankandakoglu@gmail.com>
2023-04-25 01:33:39 +0300

    morgan added

--- Planets/dist/index.js
+++ Planets/dist/index.js
@@ -5,10 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+// import morgan
+const morgan_1 = __importDefault(require("morgan"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, morgan_1.default)("dev"));
+let planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+app.post("/planets", (req, res) => {
+    const { planet } = req.body;
+    planets.push(planet);
+    res.send("Planet added");
 });
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${process.env.PORT}. Link: http://localhost:${process.env.PORT}`);
--- Planets/index.ts
+++ Planets/index.ts
@@ -1,12 +1,40 @@
 import dotenv from "dotenv";
 import express from "express";
+// import morgan
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
 
+app.use(morgan("dev"));
+
+type Planet = {
+  id: number;
+  name: string;
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/planets", (req, res) => {
+  const { planet } = req.body;
+  planets.push(planet);
+  res.send("Planet added");
 });
 
 app.listen(process.env.PORT || 3000, () => {
